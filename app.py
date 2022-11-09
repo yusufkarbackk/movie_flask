420a9f79e051d26d3e5c4382956483dec2b4a0ca
Yusuf karback <yusufkarback64@gmail.com>
2022-11-09 10:09:51 +0700

    uts pw

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
-from crypt import methods
 from flask import (Flask, render_template, request,
                    redirect, url_for, session, flash, abort)
+
 from auth import auth
+from transaksi import transaksi
 import json
 import urllib.request
 import mysql.connector
 
 app = Flask(__name__)
 app.register_blueprint(auth)
+app.register_blueprint(transaksi)
+
+app.secret_key = 'totalsecret123'
+
 
 def getMysqlConnection():
-    return mysql.connector.connect(user='root', host='localhost', port=8889, password='root', database='layar_tancep')
+    return mysql.connector.connect(user='root', host='localhost', port=8889, password='root', database='perpustakaan')
 
 
 key = '6765b9ea37def7ce46ee426d105bc4d8'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    url = f"https://api.themoviedb.org/3/movie/popular?api_key={key}&language=en-US&page=1"
+    if request.method == 'POST':
+        keyword = request.form['search_movie']
+        return redirect(url_for('search_movie', keyword=f"{keyword}"))
+    else:
+        url = f"https://api.themoviedb.org/3/movie/popular?api_key={key}&language=en-US&page=1"
 
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    dict = json.loads(data)
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        dict = json.loads(data)
 
-    return render_template('index.html', movies=dict['results'])
+        return render_template('index.html', movies=dict['results'])
 
 
-@app.route('/search_movie/')
-def search_movie():
-    keyword = request.args.get('search')
+@app.route('/search_movie/<keyword>')
+def search_movie(keyword):
     url = f'https://api.themoviedb.org/3/search/movie?api_key={key}&language=en-US&query={keyword}&page=1&include_adult=false'
     response = urllib.request.urlopen(url)
     data = response.read()
     movies = json.loads(data)
 
-    return render_template('movie_search.html', movies=movies['results'])
+    return render_template('search_result.html', movies=movies['results'])
 
 
 @app.route('/movie_detail/<int:movieId>')
@@ -58,7 +66,7 @@ def dashboard():
 def users():
     db = getMysqlConnection()
     try:
-        sqlstr = "SELECT * from users"
+        sqlstr = "SELECT * from anggota"
         cur = db.cursor()
         cur.execute(sqlstr)
         output_json = cur.fetchall()
@@ -69,48 +77,56 @@ def users():
     return render_template('users.html', data=output_json)
 
 
-@app.route('/transaksi')
-def transaksi():
+@app.route('/update_user/<int:user_id>/', methods=['GET', 'POST'])
+def update_user(user_id):
     db = getMysqlConnection()
     try:
-        sqlstr = "SELECT * from transaksi"
+        sqlstr = f"SELECT * from anggota where id_anggota={user_id}"
         cur = db.cursor()
         cur.execute(sqlstr)
-        output_json = cur.fetchall()
+        old_data = cur.fetchall()
     except Exception as e:
         print("Error in SQL:\n", e)
-    finally:
-        db.close()
-    return render_template('transaksi.html', data=output_json)
-
-
-
-
-
-@app.route('/update/<int:user_id>/', methods=['GET', 'POST'])
-def update(user_id):
-    db = getMysqlConnection()
-    status = request.form['status']
     if request.method == 'POST':
+        kode_anggota = request.form['kode_anggota']
+        nama = request.form['nama']
+        jk_anggota = request.form['jk_anggota']
+        jurusan_anggota = request.form['jurusan_anggota']
+        telpon = request.form['telpon']
+        alamat = request.form['alamat']
+
+        if len(kode_anggota) == 0:
+            kode_anggota = old_data[0][1]
+        if len(nama) == 0:
+            nama = old_data[0][2]
+        if len(jk_anggota) == 0:
+            jk_anggota = old_data[0][3]
+        if len(jurusan_anggota) == 0:
+            jurusan_anggota = old_data[0][4]
+        if len(telpon) == 0:
+            telpon = old_data[0][5]
+        if len(alamat) == 0:
+            alamat = old_data[0][6]
+
         try:
             cur = db.cursor()
-            sqlstr = f"update transaksi set status = '{status}' where id={user_id}"
+            sqlstr = f"update anggota set kode_anggota = '{kode_anggota}', nama_anggota='{nama}', jk_anggota='{jk_anggota}', jurusan_anggota='{jurusan_anggota}', no_telepon_anggota='{telpon}', alamat_anggota='{alamat}' where id_anggota={user_id}"
             cur.execute(sqlstr)
             db.commit()
-            cur.close()
             print('sukses')
+            cur.close()
         except Exception as e:
             print("Error in SQL:\n", e)
         finally:
             db.close()
-        return redirect(url_for('transaksi'))
+        return redirect(url_for('users'))
 
 
-@app.route('/update_form/<int:user_id>/')
-def update_form(user_id):
+@app.route('/user_form/<int:user_id>')
+def user_update_form(user_id):
     db = getMysqlConnection()
     try:
-        sqlstr = f"SELECT * from transaksi where id={user_id}"
+        sqlstr = f"SELECT * from anggota where id_anggota={user_id}"
         cur = db.cursor()
         cur.execute(sqlstr)
         output_json = cur.fetchall()
@@ -118,7 +134,7 @@ def update_form(user_id):
         print("Error in SQL:\n", e)
     finally:
         db.close()
-    return render_template('update.html', data=output_json)
+    return render_template('user_update_form.html', data=output_json)
 
 
 @app.route('/delete_user/<int:user_id>', methods=['GET', 'POST'])
@@ -126,7 +142,7 @@ def deleteUser(user_id):
     db = getMysqlConnection()
     try:
         cur = db.cursor()
-        sqlstr = f"delete from users where id={user_id}"
+        sqlstr = f"delete from anggota where id_anggota={user_id}"
         cur.execute(sqlstr)
         db.commit()
         cur.close()
--- auth.py
+++ auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, request, render_template, redirect, render_template, url_for
+from flask import Blueprint, request, render_template, redirect, render_template, url_for, flash, get_flashed_messages
 import mysql.connector
 
 
 def getMysqlConnection():
-    return mysql.connector.connect(user='root', host='localhost', port=8889, password='root', database='layar_tancep')
+    return mysql.connector.connect(user='root', host='localhost', port=8889, password='root', database='perpustakaan')
 
 
 auth = Blueprint('auth', __name__,)
@@ -18,18 +18,22 @@ def login():
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        nama = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
+        kode_anggota = request.form['kode_anggota']
+        nama = request.form['nama']
+        jk_anggota = request.form['jk_anggota']
+        jurusan_anggota = request.form['jurusan_anggota']
+        telpon = request.form['telpon']
+        alamat = request.form['alamat']
+
         db = getMysqlConnection()
         try:
             cur = db.cursor()
-            sqlstr = f"INSERT INTO users (nama, email, password) VALUES('{nama}', '{email}', {password})"
+            sqlstr = f"INSERT INTO anggota (kode_anggota, nama_anggota, jk_anggota, jurusan_anggota, no_telepon_anggota, alamat_anggota) VALUES('{kode_anggota}', '{nama}', '{jk_anggota}', '{jurusan_anggota}', '{telpon}', '{alamat}')"
             cur.execute(sqlstr)
             db.commit()
             cur.close()
             print('sukses')
-            output_json = cur.fetchall()
+            #output_json = cur.fetchall()
         except Exception as e:
             print("Error in SQL:\n", e)
         finally:
